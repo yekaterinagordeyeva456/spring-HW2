5a67c0a6165fd18a07df3371eab112e5706f9211
yekaterinagordeyeva456 <yges1979@gmail.com>
2023-05-09 00:13:06 +0600

    branch

---
+++ src/test/java/com/example/springhw2/SpringHw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springhw2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHw2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
